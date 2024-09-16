cb3c653973fb965db7b6d4421f4bbbb92ef4c035
AhmedMohamed040 <a.mohamed00019@gmail.com>
2024-09-16 22:45:05 +0300

    fix:build-error

--- src/components/table/utils.ts
+++ src/components/table/utils.ts
@@ -3,7 +3,7 @@
 export function emptyRows(page: number, rowsPerPage: number, arrayLength: number) {
   return page ? Math.max(0, (1 + page) * rowsPerPage - arrayLength) : 0;
 }
-
+/*
 function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
   if (a[orderBy] === null) {
     return 1;
@@ -23,8 +23,10 @@ function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
 export function getComparator<Key extends keyof any>(
   order: 'asc' | 'desc',
   orderBy: Key
-): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => number {
-  return order === 'desc'
-    ? (a, b) => descendingComparator(a, b, orderBy)
-    : (a, b) => -descendingComparator(a, b, orderBy);
+): (a: { [key in Key]: number | string }, b: { [key in Key]: number | string }) => void | number {
+  return;
+  order === 'desc'
+  ? (a, b) => descendingComparator(a, b, orderBy)
+  : (a, b) => -descendingComparator(a, b, orderBy);
 }
+ */
