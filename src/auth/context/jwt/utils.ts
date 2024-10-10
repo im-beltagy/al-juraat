838072488542fdc045ca6840373c2762dc023c5c
imbeltagy <a.mohamed00019@gmail.com>
2024-10-10 13:49:47 +0300

    fix:refresh-token

--- src/auth/context/jwt/utils.ts
+++ src/auth/context/jwt/utils.ts
@@ -39,19 +39,20 @@ export const isValidToken = (refreshTokenExpireAt: string) => {
 
 export const tokenExpired = (user?: any) => {
   // eslint-disable-next-line prefer-const
-  if (!user?.accessTokenExpireAt) {
+  const storedUser = typeof getCookie('user') === 'string' && JSON.parse(getCookie('user') as string);
+  //console.log(storedUser) ;
+
+  if (!user?.accessTokenExpireAt && storedUser) {
     return;
   }
-//const user_ = JSON.parse(JSON.stringify(getCookie('user')));
-// console.log(user) ;
-const TokenExpireAt = new Date(user?.accessTokenExpireAt) ;
-  const refreshTokenExpireAt = new Date(user?.refreshTokenExpireAt) ;
+const TokenExpireAt = new Date( user?.accessTokenExpireAt || storedUser?.accessTokenExpireAt ) ;
+  const refreshTokenExpireAt = new Date(user?.refreshTokenExpireAt || storedUser?.refreshTokenExpireAt) ;
   const now = new Date();
-
 
     if(now < refreshTokenExpireAt) {
       if (now > TokenExpireAt) {
         refreshToken(user?.refreshToken ) ;
+
       //  window.location.reload()
       }
     }else {
@@ -62,7 +63,7 @@ const TokenExpireAt = new Date(user?.accessTokenExpireAt) ;
       deleteCookie("user");
       window.location.href = paths.auth.jwt.login;
     }
-  //  setTimeout(tokenExpired, 2 * 60 * 1000);
+ setInterval(tokenExpired, 3 * 60 * 1000);
 
 
 };
@@ -100,9 +101,9 @@ export const refreshToken = async (token:string) => {
   const {accessToken } = res.data;
   axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
   sessionStorage.setItem('accessToken', accessToken);
-  sessionStorage.setItem('user', res.data);
+  sessionStorage.setItem('user', JSON.stringify(res.data));
   setCookie("accessToken", accessToken);
-  setCookie("user", res.data);
+  setCookie("user", JSON.stringify(res.data));
 };
 
 export const getAccessToken = () => {};
